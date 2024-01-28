f84aa0ff6cae7fcb72848e8e31e6f6a2e2ea6e0d
FROI <johnfroilanlluz2@gmail.com>
2024-01-29 02:14:06 +0800

    feat: :sparkles:

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent {
+  activeLink: string = ''; // To keep track of the active link
 
+  setActiveLink(link: string): void {
+    this.activeLink = link;
+  }
 }
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,39 +1,58 @@
 import { Component, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Project } from '../_models/Project';
-import { ProjectsService } from '../_services/projects.service'; 
+import { ProjectsService } from '../_services/projects.service';
 import { Tag } from '../_models/Tag';
-
-
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss'],
 })
-export class PortfolioComponent implements OnInit{
-
+export class PortfolioComponent implements OnInit {
   projects = {} as Project[];
   isCollapsed = true;
   typescript = false;
+  angular = false;
+  python = false;
+  javascript = false;
+  flask = false;
+  nodejs = false;
+  electron = false;
 
-
-
-
-  constructor(private titleService: Title,private projectService: ProjectsService ) {
+  constructor(
+    private titleService: Title,
+    private projectService: ProjectsService
+  ) {
     this.titleService.setTitle('John Froilan | Portfolio');
   }
   ngOnInit(): void {
     this.projects = this.projectService.GetProjects();
   }
-  Filter(){
-    let filterTags:Tag[] = [];
+  Filter() {
+    let filterTags: Tag[] = [];
 
-    if(this.typescript){
+    if (this.typescript) {
       filterTags.push(Tag.TYPESCRIPT);
     }
-
+    if (this.angular) {
+      filterTags.push(Tag.ANGULAR);
+    }
+    if (this.python) {
+      filterTags.push(Tag.PYTHON);
+    }
+    if (this.javascript) {
+      filterTags.push(Tag.JAVASCRIPT);
+    }
+    if (this.flask) {
+      filterTags.push(Tag.FLASK);
+    }
+    if (this.nodejs) {
+      filterTags.push(Tag.NODEJS);
+    }
+    if (this.electron) {
+      filterTags.push(Tag.ELECTRON);
+    }
     this.projects = this.projectService.GetProjectByFilter(filterTags);
   }
-  
 }
