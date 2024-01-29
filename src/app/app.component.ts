d25f2e997c821ddd6213cbb27320ebda4da5c13a
FROI <johnfroilanlluz2@gmail.com>
2024-01-29 15:49:55 +0800

    feat: :dizzy:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import * as AOS from 'aos';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'sampleportfolio';
+  ngOnInit() {
+    AOS.init(); //AOS - 2
+    AOS.refresh(); //refresh method is called on window resize and so on, as it doesn't require to build new store with AOS elements and should be as light as possible.
+  }
 }
